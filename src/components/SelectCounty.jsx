9a3730fcb890d2b125ff690d5adfc9d870993fe9
swc9803 <swc9803@gmail.com>
2024-01-18 20:05:17 +0900

    모든 국가 fetch

--- src/components/SelectCounty.jsx
+++ src/components/SelectCounty.jsx
@@ -1,37 +1,36 @@
-import React, { useState } from 'react';
 import useFetch from '../hooks/useFetch.jsx';
 
 const SelectCounty = () => {
   const apiKey = import.meta.env.VITE_POPULATION_API_KEY;
 
-  const [selectedCountry, setSelectedCountry] = useState('가나');
-  const url = `http://apis.data.go.kr/1262000/CountryPopulationService2/getCountryPopulationList2?serviceKey=${apiKey}&cond[country_nm::EQ]=${selectedCountry}`;
-
+  const url = `http://apis.data.go.kr/1262000/CountryPopulationService2/getCountryPopulationList2?serviceKey=${apiKey}&numOfRows=500`;
   const data = useFetch(url);
-  const latestData = data ? data.data[0] : '';
 
-  const changeCountry = (country) => {
-    setSelectedCountry(country);
-  };
+  const evenIndexData = data ? data.data.filter((_, index) => index % 2 === 0) : [];
 
   return (
     <>
-      {!latestData ? (
+      <button onClick={() => changeCountry('가나')}>가나</button>
+      <button onClick={() => changeCountry('영국')}>영국</button>
+      <button onClick={() => changeCountry('일본')}>일본</button>
+      {!evenIndexData.length ? (
         <>
           <p>loading</p>
         </>
       ) : (
         <>
-          <p>Selected Country: {latestData.country_nm}</p>
-          <button onClick={() => changeCountry('가나')}>가나</button>
-          <button onClick={() => changeCountry('영국')}>영국</button>
-          <button onClick={() => changeCountry('일본')}>일본</button>
-          <ul>
-            <li>
-              <p>인구수: {latestData.popltn_cnt}</p>
-              <p>증감률: {latestData.popltn_growth}</p>
-            </li>
-          </ul>
+          {evenIndexData.map((item, index) => (
+            <div key={index}>
+              <ul>
+                <li>
+                  <p>Selected Country: {item.country_nm}</p>
+                  <p>population: {item.popltn_cnt}</p>
+                  <p>population rate: {item.popltn_growth}</p>
+                  <br />
+                </li>
+              </ul>
+            </div>
+          ))}
         </>
       )}
     </>
