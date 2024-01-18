def893cc514c758140216f6c203ad5f78f53990a
swc9803 <swc9803@gmail.com>
2024-01-19 03:38:08 +0900

    chart components화 및 props 전달

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import './App.css';
+import { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import SelectCountry from './components/SelectCountry';
 import Chart from './components/Chart';
 
 const App = () => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
+  const onChangeCountry = (country) => {
+    setSelectedCountry(country);
+  };
+
   return (
     <div>
-      <SelectCountry />
+      <SelectCountry changeCountry={onChangeCountry} />
       <Canvas
         shadows
         camera={{
           position: [7, 7, 0],
         }}
       >
-        <Chart />
+        <Chart selectedCountry={selectedCountry} />
       </Canvas>
     </div>
   );
--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -1,43 +1,36 @@
 import useFetch from '../hooks/useFetch.jsx';
 
-const SelectCounty = () => {
+const SelectCountry = ({ changeCountry }) => {
   const apiKey = import.meta.env.VITE_POPULATION_API_KEY;
-
   const url = `http://apis.data.go.kr/1262000/CountryPopulationService2/getCountryPopulationList2?serviceKey=${apiKey}&numOfRows=500`;
   const data = useFetch(url);
 
   const evenIndexData = data ? data.data.filter((_, index) => index % 2 === 0) : [];
 
+  const selectCountry = (country) => {
+    changeCountry(country);
+  };
+
   return (
     <>
-      <button onClick={() => changeCountry('가나')}>가나</button>
-      <button onClick={() => changeCountry('영국')}>영국</button>
-      <button onClick={() => changeCountry('일본')}>일본</button>
-      {!evenIndexData.length ? (
-        <>
-          <p>loading</p>
-        </>
-      ) : (
-        <>
-          {evenIndexData.map((item, index) => (
-            <div key={index}>
-              <ul>
-                <li>
-                  <p>Selected Country : {item.country_nm}</p>
-                  <p>population : {item.popltn_cnt !== null ? item.popltn_cnt : 0}</p>
-                  <p>
-                    population growth rate :
-                    {item.popltn_growth !== null ? item.popltn_growth : 0}
-                  </p>
-                  <br />
-                </li>
-              </ul>
-            </div>
-          ))}
-        </>
-      )}
+      {evenIndexData.map((item, index) => (
+        <div key={index}>
+          <ul>
+            <li>
+              <p>Selected Country : {item.country_nm}</p>
+              <p>population : {item.popltn_cnt !== null ? item.popltn_cnt : 0}</p>
+              <p>
+                population growth rate :
+                {item.popltn_growth !== null ? item.popltn_growth : 0}
+              </p>
+              <button onClick={() => selectCountry(item.country_nm)}>Select</button>
+              <br />
+            </li>
+          </ul>
+        </div>
+      ))}
     </>
   );
 };
 
-export default SelectCounty;
+export default SelectCountry;
